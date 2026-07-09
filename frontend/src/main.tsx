53bafa8a78fb04e4d4b3e2c03dc620cd477bc81d
Kiyalan <kiyyaforever007@gmail.com>
2026-07-09 16:26:49 +0800

    feat(frontend-theme): Reorder theme code
    1. 整合了 /frontend/src/styles/github-theme.css，/frontend/src/
       App.css 中的与 github 风格设计有关的内容到 index.css 并删除了
       三者中的 react 模板代码
    2. 删除了部分 react 模板残留的资源文件，包括 /frontend/src/assets/*
       ，/frontend/public/icons.svg

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './styles/github-theme.css'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
