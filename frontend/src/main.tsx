d7ff30dcbfebfd03980996bfca3f567147065afb
Kiyalan <kiyyaforever007@gmail.com>
2026-07-13 18:32:42 +0800

    feat(admin-merge): Arrange admin and github theme
    1. 把 github 风格从 index.css 中抽离出来形成 github-theme.css，和
       admin-theme.css 并列。index.css，github-theme.css，admin-theme.css
       三者统一在 main.tsx 中进行导入

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import './styles/github-theme.css'
+import './styles/admin-theme.css'
 
 const queryClient = new QueryClient({
   defaultOptions: {
